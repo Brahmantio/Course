bca518981937c579c42d717123fdaa8c9400018f
Brahmantio <151048256+Brahmantio@users.noreply.github.com>
2025-06-21 12:56:48 +0700

    Update porto.py

--- porto.py
+++ porto.py
@@ -94,7 +94,7 @@ with tab1:
     
     # Predict Button
     if st.button('Predict Now!'):
-            model = "mount/src/course/modeldqlab.pkl"
+            model = "course/modeldqlab.pkl"
             with open(model, 'rb') as file:
                     loaded_model = pickle.load(file)
                     prediction = loaded_model.predict(features)
