177a6ce6d6bd9d7095276deab11f9a889c9734f7
Brahmantio <151048256+Brahmantio@users.noreply.github.com>
2025-06-23 11:29:40 +0700

    Update porto.py

--- porto.py
+++ porto.py
@@ -95,10 +95,10 @@ with tab1:
     # Predict Button
 if st.button('Predict Now!'):
             #model_loc = '/mount/src/course/modeldqlab.pkl'
-        with open('modeldqlab.pkl','rb') as file:
-                model = pickle.load(file)
+with open('modeldqlab.pkl','rb') as file:
+        model = pickle.load(file)
         prediction = model.predict(features)
-        with st.spinner('Wait for it...'):
+with st.spinner('Wait for it...'):
                 time.sleep(4)
                 st.success(f"Hasil prediksiku: harga rumah seharga ${prediction[0]:,.2f}")
         
